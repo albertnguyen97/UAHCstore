f4c68db3d946d52331ead4526ec56838c269961a
albertnguyen97 <chaunm.hmc@gmail.com>
2023-08-29 02:00:10 +0700

    add edit

--- item/views.py
+++ item/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Item
 from django.contrib.auth.decorators import login_required
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 from django.shortcuts import render, redirect
+
+
 # Create your views here.
 
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
-    return render(request, 'item/detail.html',{
+    return render(request, 'item/detail.html', {
         'item': item,
         'related_items': related_items
     })
 
 
+@login_required
 def new(request):
     form = NewItemForm()
     if request.method == 'POST':
@@ -26,9 +29,26 @@ def new(request):
             return redirect('item:detail', pk=item.id)
         else:
             form = NewItemForm()
-    return render(request, 'item/form.html',{
+    return render(request, 'item/form.html', {
         'form': form,
         'title': 'New item',
+    })
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, create_by=request.user)
+    form = EditItemForm()
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('item:detail', pk=item.id)
+        else:
+            form = EditItemForm(instance=item)
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit item',
     })
 
 
