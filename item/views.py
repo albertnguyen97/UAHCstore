bd95915ae9df4eb970f1738b9122003ed0a17071
albertnguyen97 <chaunm.hmc@gmail.com>
2023-08-29 23:55:22 +0700

    add search

--- item/views.py
+++ item/views.py
@@ -7,7 +7,9 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
-
+def items(request):
+    items = Item.objects.filter(is_sold=False)
+    return render(request, 'item/items.html')
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
