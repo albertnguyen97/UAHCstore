61bdcbb3ec77e2004656f463972771950379db70
albertnguyen97 <chaunm.hmc@gmail.com>
2023-08-30 00:04:09 +0700

    update views

--- item/views.py
+++ item/views.py
@@ -9,7 +9,9 @@ from django.shortcuts import render, redirect
 
 def items(request):
     items = Item.objects.filter(is_sold=False)
-    return render(request, 'item/items.html')
+    return render(request, 'item/items.html', {
+        'items': items
+    })
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
